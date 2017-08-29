5fdd2ea65a48aba27b1bc359a536a2cac50155cb
Volodymyr Bendiuga <volodymyr.bendiuga@westermo.se>
2017-08-29 13:11:20 +0200

    ip: add alias
    Signed-off-by: Volodymyr Bendiuga <volodymyr.bendiuga@westermo.se>

--- ip/ipaddress.c
+++ ip/ipaddress.c
@@ -86,6 +86,7 @@ static void usage(void)
 	fprintf(stderr, "IFADDR := PREFIX | ADDR peer PREFIX\n");
 	fprintf(stderr, "          [ broadcast ADDR ] [ anycast ADDR ]\n");
 	fprintf(stderr, "          [ label IFNAME ] [ scope SCOPE-ID ]\n");
+	fprintf(stderr, "          [ alias IFALIAS ]\n");
 	fprintf(stderr, "SCOPE-ID := [ host | link | global | NUMBER ]\n");
 	fprintf(stderr, "FLAG-LIST := [ FLAG-LIST ] FLAG\n");
 	fprintf(stderr, "FLAG  := [ permanent | dynamic | secondary | primary |\n");
@@ -1184,6 +1185,8 @@ int print_addrinfo(const struct sockaddr_nl *who, struct nlmsghdr *n,
 	}
 	if (ifa_flags)
 		fprintf(fp, "flags %02x ", ifa_flags);
+	if (rta_tb[IFA_ALIAS])
+		fprintf(fp, "%s", rta_getattr_str(rta_tb[IFA_ALIAS]));
 	if (rta_tb[IFA_LABEL])
 		fprintf(fp, "%s", rta_getattr_str(rta_tb[IFA_LABEL]));
 	if (rta_tb[IFA_CACHEINFO]) {
@@ -1940,6 +1943,9 @@ static int ipaddr_modify(int cmd, int flags, int argc, char **argv)
 		} else if (strcmp(*argv, "dev") == 0) {
 			NEXT_ARG();
 			d = *argv;
+		} else if (strcmp(*argv, "alias") == 0) {
+			NEXT_ARG();
+			addattr_l(&req.n, sizeof(req), IFA_ALIAS, *argv, strlen(*argv)+1);
 		} else if (strcmp(*argv, "label") == 0) {
 			NEXT_ARG();
 			l = *argv;
